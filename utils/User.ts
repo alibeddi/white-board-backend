9a6fccb1e71b8a3747c7f29563440e9ad29ff5cc
ali beddi <alibeddi@takiacademyteam.com>
2024-02-25 21:34:24 +0100

    feat(joinRoom): add users logic

--- utils/User.ts
+++ utils/User.ts
@@ -1,35 +1,45 @@
-const users = [];
-
-// Join user to chat
-const userJoin = (id, username, room, host, presenter) => {
-  const user = { id, username, room, host, presenter };
-
-  users.push(user);
-  return user;
-};
-// User leaves chat
-const userLeave = (id) => {
-  const index = users.findIndex((user) => user.id === id);
-
-  if (index !== -1) {
-    return users.splice(index, 1)[0];
+interface User {
+    id: string;
+    username: string;
+    room: string;
+    host: boolean;
+    presenter: boolean;
   }
-};
-
-//get users
-const getUsers = (room) => {
-  const RoomUsers = [];
-  users.map((user) => {
-    if (user.room == room) {
-      RoomUsers.push(user);
+  
+  const users: User[] = [];
+  
+  // Join user to chat
+  const userJoin = (id: string, username: string, room: string, host: boolean, presenter: boolean): User => {
+    const user: User = { id, username, room, host, presenter };
+  
+    users.push(user);
+    return user;
+  };
+  
+  // User leaves chat
+  const userLeave = (id: string): User | undefined => {
+    const index = users.findIndex((user) => user.id === id);
+  
+    if (index !== -1) {
+      return users.splice(index, 1)[0];
     }
-  });
-
-  return RoomUsers;
-};
-
-module.exports = {
-  userJoin,
-  userLeave,
-  getUsers,
-};
+  };
+  
+  // Get users
+  const getUsers = (room: string): User[] => {
+    const roomUsers: User[] = [];
+    users.forEach((user) => {
+      if (user.room === room) {
+        roomUsers.push(user);
+      }
+    });
+  
+    return roomUsers;
+  };
+  
+  export {
+    userJoin,
+    userLeave,
+    getUsers,
+  };
+  
